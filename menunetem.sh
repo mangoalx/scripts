a8b4c056c112aad3be6f22e9c74d818330f80c81
John Xu <john.xu@videri.com>
2020-02-10 16:41:04 +0000

    ver0.1 menunetem.sh usable, up/down stream limit could stuck

--- menunetem.sh
+++ menunetem.sh
@@ -6,21 +6,31 @@
 # Version 0.1
 #	- basic menu, tc control commands
 #	- associative array for parameters edit
-#	* combine netem and tbf togather 
-#	* download/upload control respectively
-#	* <a> for all parameters edit
-
+#	- combine netem and tbf togather
+#	- modify latency for tbf to limit & default to 3000
+#	- remove latency(or limit) and correlation parameter from up/down limit
+#	- use limit of fixed 3000 for up/download limit, use correlation from delayVars
+#	- download/upload control respectively
+#	- <a> for all parameters edit
+#	- display current value and <Enter> to keep current value
+#	- <Enter> to accept and apply
+#
+#
+#	* up/down stream limit could cause network blocked somehow, when this happens just stop netem to recover. 
+#	  not sure what setting could cause this, needs to be investigated in the future
+#============================================================================
 TAG="${0##*/}"
 Version="0.1"
 ######################## variables #######################
 # for delayJitter
 declare -A delayVars
-delayVars=(["Delay(ms)"]=100 ["Jitter(ms)"]=500 ["Ncorrelation(%)"]=25)
+delayVars=(["Delay(ms)"]=100 ["Jitter(ms)"]=500 ["Correlation(%)"]=25)
 #dDelay=100
 #dJitter=500
 #dCorrelation=25
 #dVars=(100 500 25)
 #dVarNames=("Delay(ms)" "Jitter(ms)" "Correlation(%)")
+
 
 # for packetLoss
 declare -A lossVars=(["Loss(%)"]=20)
@@ -29,13 +39,13 @@ declare -A lossVars=(["Loss(%)"]=20)
 declare -A corrVars=(["Corrupt(%)"]=15)
 
 # for downloadLimit
-declare -A downVars=(["Rate(kbps)"]=1000 ["Burst"]=1600 ["Alatency"]=20000 ["Delay(ms)"]=100 ["Jitter(ms)"]=50 ["Ncorrelation(%)"]=25 ["Loss(%)"]=10 ["Corrupt(%)"]=5)
+declare -A downVars=(["Rate(kbps)"]=10000 ["Burst"]=1600 ["Delay(ms)"]=10 ["Jitter(ms)"]=5 ["Loss(%)"]=1 ["Corrupt(%)"]=0)
 
 # for uploadLimit
-declare -A upVars=(["Rate(kbps)"]=1000 ["Burst"]=1600 ["Alatency"]=20000 ["Delay(ms)"]=100 ["Jitter(ms)"]=50 ["Ncorrelation(%)"]=25 ["Loss(%)"]=10 ["Corrupt(%)"]=5)
+declare -A upVars=(["Rate(kbps)"]=10000 ["Burst"]=1600 ["Delay(ms)"]=10 ["Jitter(ms)"]=5 ["Loss(%)"]=1 ["Corrupt(%)"]=0)
 
 # for bandVars
-declare -A bandVars=(["Rate(kbps)"]=1000 ["Burst"]=1600 ["Alatency"]=20000)
+declare -A bandVars=(["Rate(kbps)"]=1000 ["Burst"]=1600 ["Limit"]=16000)
 ######################## functions #######################
 version()
 {
@@ -50,6 +60,11 @@ usage()
 EOF
 }
 
+#isNaturalNumber()
+#{
+#	[[ ${1} = ~ ^[0-9]+$ ]]
+#}
+re='^[0-9]+$'
 editParas()
 {
 	declare -n vars=$1
@@ -62,7 +77,7 @@ editParas()
 		done
 		echo
 
-		read -n 1 -p 'Input the 1st letter of the parameter you want to change, <Enter> to skip:' input
+		read -n 1 -p 'Input the 1st letter of the parameter you want to edit, "a" for all, <Enter> to accept & apply: ' input
 		echo
 		if [[ -z "$input" ]]; then
 			break
@@ -70,9 +85,14 @@ editParas()
 			shopt -s nocasematch
 #			${var1^^} to change to uppercase
 			for key in ${!vars[@]}; do
-				if [[ $input == ${key:0:1} ]]; then
-					read -p "Enter new value for ${key}: " value
-					vars[$key]=$value
+				if [[ $input ==  'a'|| $input == ${key:0:1} ]]; then
+					read -p "Enter new value for ${key}(${vars[$key]}): " value
+					if [[ -n "$value" ]]; then
+						if [[ $value =~ $re ]]; then		#if it a pure number
+							vars[$key]=$value
+						else echo "Invalid data, ejected!"
+						fi
+					fi
 					local found=1
 					continue
 				fi
@@ -106,15 +126,15 @@ showNetem()
 bandwidth()
 {
 	editParas bandVars
-	tc qdisc add dev eth0 root tbf rate ${bandVars["Rate(kbps)"]}kbit burst ${bandVars["Burst"]} latency ${bandVars["Alatency"]}
-	tc qdisc add dev eth1 root tbf rate ${bandVars["Rate(kbps)"]}kbit burst ${bandVars["Burst"]} latency ${bandVars["Alatency"]}
+	tc qdisc add dev eth0 root tbf rate ${bandVars["Rate(kbps)"]}kbit burst ${bandVars["Burst"]} limit ${bandVars["Limit"]}
+	tc qdisc add dev eth1 root tbf rate ${bandVars["Rate(kbps)"]}kbit burst ${bandVars["Burst"]} limit ${bandVars["Limit"]}
 }
 delayJitter()
 {
 	editParas delayVars
 #	tc qdisc add dev eth0 root netem delay ${dDelay}ms ${dJitter}ms ${dCorrelation}%
-	tc qdisc add dev eth0 root netem delay ${delayVars["Delay(ms)"]}ms ${delayVars["Jitter(ms)"]}ms ${delayVars["Ncorrelation(%)"]}%
-	tc qdisc add dev eth1 root netem delay ${delayVars["Delay(ms)"]}ms ${delayVars["Jitter(ms)"]}ms ${delayVars["Ncorrelation(%)"]}%
+	tc qdisc add dev eth0 root netem delay ${delayVars["Delay(ms)"]}ms ${delayVars["Jitter(ms)"]}ms ${delayVars["Correlation(%)"]}%
+	tc qdisc add dev eth1 root netem delay ${delayVars["Delay(ms)"]}ms ${delayVars["Jitter(ms)"]}ms ${delayVars["Correlation(%)"]}%
 #	tc qdisc add dev eth1 root netem delay ${dVars[0]}ms ${dVars[1]}ms ${dVars[2]}%
 #	tc qdisc add dev eth1 root netem delay ${dDelay}ms ${dJitter}ms ${dCorrelation}%
 }
@@ -133,15 +153,15 @@ packetCorrupt()
 downloadLimit()
 {
 	editParas downVars
-	tc qdisc add dev eth0 root handle 1: netem delay ${downVars["Delay(ms)"]}ms ${downVars["Jitter(ms)"]}ms ${downVars["Ncorrelation(%)"]}% loss ${downVars["Loss(%)"]} corrupt ${downVars["Corrupt(%)"]}
-	tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate ${downVars["Rate(kbps)"]}kbit burst ${downVars["Burst"]} latency ${downVars["Alatency"]}
+	tc qdisc add dev eth0 root handle 1: netem delay ${downVars["Delay(ms)"]}ms ${downVars["Jitter(ms)"]}ms ${delayVars["Correlation(%)"]}% loss ${downVars["Loss(%)"]} corrupt ${downVars["Corrupt(%)"]}
+	tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate ${downVars["Rate(kbps)"]}kbit burst ${downVars["Burst"]} limit 3000 # ${bandVars["Limit"]}
 
 }
 uploadLimit()
 {
 	editParas upVars
-	tc qdisc add dev eth1 root handle 1: netem delay ${upVars["Delay(ms)"]}ms ${upVars["Jitter(ms)"]}ms ${upVars["Ncorrelation(%)"]}% loss ${upVars["Loss(%)"]} corrupt ${upVars["Corrupt(%)"]}
-	tc qdisc add dev eth1 parent 1:1 handle 10: tbf rate ${upVars["Rate(kbps)"]}kbit burst ${upVars["Burst"]} latency ${upVars["Alatency"]}
+	tc qdisc add dev eth1 root handle 1: netem delay ${upVars["Delay(ms)"]}ms ${upVars["Jitter(ms)"]}ms ${delayVars["Correlation(%)"]}% loss ${upVars["Loss(%)"]} corrupt ${upVars["Corrupt(%)"]}
+	tc qdisc add dev eth1 parent 1:1 handle 10: tbf rate ${upVars["Rate(kbps)"]}kbit burst ${upVars["Burst"]} limit 3000 #${bandVars["Limit"]}
 #	tc qdisc add dev eth1 root handle 1: tbf rate ${upVars["Rate(kbps)"]}kbit burst ${upVars["Burst"]} latency ${upVars["Alatency"]}
 #	tc qdisc add dev eth1 parent 1:1 handle 10: netem delay ${upVars["Delay(ms)"]}ms ${upVars["Jitter(ms)"]}ms ${upVars["Ncorrelation(%)"]}% loss ${upVars["Loss(%)"]} corrupt ${upVars["Corrupt(%)"]}
 }
