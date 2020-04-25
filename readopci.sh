fd489bcffef242a21a744a7c220895b2c9798504
John Xu <john.xu@videri.com>
2020-04-25 00:42:48 -0400

    for -p output search for index of %CPU to locate cpu load data, read all i2c sensors

--- readopci.sh
+++ readopci.sh
@@ -6,8 +6,12 @@
 #	- Based on readTemp.sh
 #	- -c for overall cpu load
 #	- -p for pid/program specific cpu load
-#	* read sensors temp/humidity, tmp275, als, etc.
-#	* read ina220 sensors, specify address
+#		search for index to find %CPU so get correct value
+#	- read sensors temp/humidity, tmp275, als, etc.
+#	- read ina220 sensors, specify address
+#	* loop reading
+#	* delay time setting
+#	* option to write to file
 #=========================================================================
 TAG="${0##*/}"				#get the base name of itself
 ############ Functions
@@ -21,7 +25,7 @@ usage()	#edit
 {
 	version
 	cat << EOF
-	USAGE: $TAG [-hvfc] [-p program] [-u username] [-w password] [-b i2cbus] [-i "i2caddr0 i2caddr1 ..."] [-a] <ipaddress>
+	USAGE: $TAG [-hvfc] [-p program] [-u username] [-w password] [-m -t -l] [-b i2cbus] [-i "i2caddr0 i2caddr1 ..."] [-a] <ipaddress>
 		-h or --help to display this message
 		-v or --version to display version information
 		-f or --fieldname to output field names
@@ -34,6 +38,10 @@ usage()	#edit
 
 		-b or --i2cbus to specify i2cbus number (default 6. for 8u2i and cc48smi it should be 5)
 		-i or --ina220 to specify reading an ina220 sensor (hex i2c address) Voltage, Current, Power. 
+		
+		-m or --humidity to read temperature/humidity from hdc1080 0x40 
+		-t or --temperature to read temperature sensor tmp275 0x48
+		-l or --light to read light sensor ALS 0x44
 EOF
 }
 
@@ -46,54 +54,45 @@ outfieldname()
 	if [ "$program" != "" ]; then
 		output="${output}, ${program}"
 	fi
+	if [ "$humidity" = "1" ]; then
+		output="${output}, Temp1080, Humi1080"
+	fi
+	if [ "$temperature" = "1" ]; then
+		output="${output}, Tmp275"
+	fi
+	if [ "$light" = "1" ]; then
+		output="${output}, Illuminance, IR_intensity"
+	fi
 	if [ "$i2caddr" != "" ]; then
 		for addr in ${i2caddr}; do
-			output="${output}, V_${addr}, C_${addr}"
+			output="${output}, V_${addr}, I_${addr}"
 		done
 	fi
 
 	echo $output
 }
 
-convertecbdata()	#edit
+#index=
+getIndexOfWord () 			# deprecated, find out the index of a word in a string (word, string)
 {
-	local hexint=$(echo $1|cut -b 15,17)
-	local data=$((0x${hexint}))
-	local hexfrac=$(echo $1|cut -b 18)
-	if [ "$hexfrac" == "8" ]; then
-		data="${data}.5"
-	fi
-	echo $data
+	echo "Entered getIndex"
+	echo $1
+	echo $2
+	strarray=($2)
+	cnt=0; for w in "${strarray[@]}"; do
+#        [[ $w == "$1" ]] && echo $cnt && break
+        [[ $w == "$1" ]] && index=$cnt && break
+		echo $w
+        ((++cnt))
+    done
+}
+strindex() { 
+  x="${1%%$2*}"
+  [[ "$x" = "$1" ]] || echo "${#x}"
 }
 
-getecb()	#edit
-{
-	local message=
-	case $1 in
-		00)	message=$($adbc 'nuvoisp -a "AA 10 00 01 00 00 00" |grep "<=="');;
-		01)	message=$($adbc 'nuvoisp -a "AA 10 01 01 00 00 00" |grep "<=="');;
-		02)	message=$($adbc 'nuvoisp -a "AA 10 02 01 00 00 00" |grep "<=="');;
-		03)	message=$($adbc 'nuvoisp -a "AA 10 03 01 00 00 00" |grep "<=="');;
-		04)	message=$($adbc 'nuvoisp -a "AA 10 04 01 00 00 00" |grep "<=="');;
-		05)	message=$($adbc 'nuvoisp -a "AA 10 05 01 00 00 00" |grep "<=="');;
-		06)	message=$($adbc 'nuvoisp -a "AA 10 06 01 00 00 00" |grep "<=="');;
-		*) message=
-	esac
+#getWordByIndex()			#
 
-#	local message=$($adbc 'nuvoisp -a "AA 10 ${1} 01 00 00 00" |grep "<=="')
-#	local message=$($adbc 'nuvoisp -a "AA 10 $1 01 00 00 00"')
-#	echo $message >&2
-	echo $( convertecbdata "$message" )
-}
-
-convertprttemp()	#edit
-{
-	local data=$1
-	local result=$(($data*1650/65536-400))		#to get 1 digit decimal, multiple the formula with 10
-	echo "${result:0:-1}.${result: -1}"			#then display the point before last digit
-}
-
-#ecbSensors=("00" "01" "02" "03" "04" "05")
 address=
 cpuUsage=
 field=
@@ -102,6 +101,9 @@ username="ubuntu"
 password="ubuntu"
 i2cbus="6"
 i2caddr=
+humidity=
+temperature=
+light=
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -116,6 +118,15 @@ while [ "$1" != "" ]; do
                                 ;;
 		-c | --cpu)
 								cpuUsage=1 
+								;;
+		-m | --humidity)
+								humidity=1 
+								;;
+		-t | --temperature)
+								temperature=1 
+								;;
+		-l | --light)
+								light=1 
 								;;
 		-p | --program)			shift
 								program=$1
@@ -166,25 +177,54 @@ if [ "$cpuUsage" = "1" ]; then
 fi
 
 if [ "$program" != "" ]; then
-#	"top -n 1 -d 20" 2>&1 |grep 20190|awk '{printf $9 "\n"}'
-	v0=$($sshc "top -n 1|grep $program" 2>&1|grep $program|awk '{print $10}')
+	message=$($sshc "top -b -n 1" 2>&1|grep -E "%CPU|$program")			# -b is important, without it top output escaped sequence, thus could find the exact index
+#	echo "$message"
+#	index=$(getIndexOfWord "%CPU" $(echo "$message"|head -n 1))
+	message1=$(echo "$message"|grep %CPU)
+	message2=$(echo "$message"|grep $program)
+#	echo "$message2"
+	index=$(strindex "${message1}" "%CPU")
+#	getIndexOfWord "%CPU" "${message1}"
+#	echo $index
+	if [ "$index" != "" ]; then
+		((--index))
+		v0="${message2:$index:5}"
+	else
+		v0=""
+	fi
 	output="${output}, $v0"
 fi
 
+comm=
+if [ "$humidity" = "1" ]; then
+	comm="cat /sys/class/i2c-dev/i2c-$i2cbus/device/$i2cbus-0040/iio\:device*/in_humidityrelative_raw;cat /sys/class/i2c-dev/i2c-$i2cbus/device/$i2cbus-0040/iio\:device*/in_temp_raw"
+fi
+if [ "$temperature" = "1" ]; then
+	if [ "$comm" != "" ]; then 
+		comm="${comm};"
+	fi
+	comm="${comm}cat /sys/class/i2c-dev/i2c-$i2cbus/device/$i2cbus-0048/hwmon/hwmon*/temp1_input"
+fi
+if [ "$light" = "1" ]; then
+	if [ "$comm" != "" ]; then 
+		comm="${comm};"
+	fi
+	comm="${comm}cat /sys/class/i2c-dev/i2c-$i2cbus/device/$i2cbus-0044/iio\:device*/in_illuminance0_input;cat /sys/class/i2c-dev/i2c-$i2cbus/device/$i2cbus-0044/iio\:device*/in_intensity_ir_raw"
+fi
+
 if [ "$i2caddr" != "" ]; then
-#	"cat /sys/class/i2c-dev/i2c-5/device/5-0041/hwmon/hwmon*/curr1_input"
-	comm=
-#	i2carray=($i2caddr)
-#	for addr in ${i2carray[@]}; do
-#	i2caddr=${i2caddr,,}			#to convert all to lower case
 	for addr in ${i2caddr}; do
 		if [ "$comm" != "" ]; then 
 			comm="${comm};" 
 		fi
 		comm="${comm} cat /sys/class/i2c-dev/i2c-${i2cbus}/device/${i2cbus}-00${addr}/hwmon/hwmon*/in1_input; cat /sys/class/i2c-dev/i2c-${i2cbus}/device/${i2cbus}-00${addr}/hwmon/hwmon*/curr1_input"
 	done
+fi
+
+if [ "$i2caddr" != "" ]; then
 #	echo $comm
-	message=$($sshc $comm 2>&1|grep -vE "Warning|Connection"|tr ',' ';'|tr '\r\n' ', ')  #convert ',' so each data occupy 1 position in CSV output
+	message=$($sshc $comm 2>&1|grep -vE "Warning|Connection"|tr ',' ';'|tr '\r\n' ', '|sed 's/, $//')  
+# convert ',' so each data occupy 1 position in CSV output. replace newline with comma, and remove the last one
 #	echo $message
 #	echo
 
